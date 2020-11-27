cb8ae2a62b523e80bf7633cdd038dd91ede51b7e
Joel <locobaloco@gmail.com>
2020-11-27 23:31:55 +0800

    Add widgets to sidebar

--- src/app.py
+++ src/app.py
@@ -2,8 +2,46 @@ import streamlit as st
 import pydeck as pdk
 
 MAPBOX_STYLE = 'mapbox://styles/caisho/ckhzpiwfm1x7419pujepchs2x'
+PROPERTY_TYPES = [
+    'Apartment',
+    'Condominium',
+    'Detached',
+    'Executive Condominium',
+    'Semi-detached',
+    'Strata Detached',
+    'Strata Semi-detached',
+    'Strata Terrace',
+    'Terrace',
+]
 
-st.sidebar.markdown('### Map Layers')
+# Sidebar
+st.sidebar.subheader('Map Layers')
+start_date = st.sidebar.selectbox(
+    label='Start date of transactions',
+    options=['2017', '2018', '2019', '2020']
+)
+
+end_date = st.sidebar.selectbox(
+    label='End date of transactions',
+    options=['2017', '2018', '2019', '2020'],
+    index=3
+)
+
+property_type = st.sidebar.multiselect(
+    label='Select property type(s)',
+    options=PROPERTY_TYPES,
+    default=PROPERTY_TYPES,
+)
+
+# area 
+# type of sale
+# tenure
+# type of area (Land, Strata)
+
+
+# Body
+st.title('Singapore District Map')
+st.text('Hello Joni')
 
 st.pydeck_chart(pdk.Deck(
     map_style=MAPBOX_STYLE,
